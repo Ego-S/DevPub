4e7518c592b571a56ffdee209d09c3e010080c7f
Ego-S <sizenko.egor@gmail.com>
2021-01-18 18:47:40 +0200

    encoding db error

--- src/main/resources/db/changelog/v1.0/01-fill-global-settings-table.sql
+++ src/main/resources/db/changelog/v1.0/01-fill-global-settings-table.sql
@@ -1,10 +1,5 @@
-DELETE FROM global_settings
-
-GO
-
 INSERT INTO global_settings (code, name, value)
 VALUES('MULTIUSER_MODE','Многопользовательский режим','YES'),
 ('POST_PREMODERATION','Премодерация постов','NO'),
 ('STATISTIC_IS_PUBLIC','Показывать всем статистику блога','YES')
-
 GO
